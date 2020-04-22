9b2ecc9d34fcf47274a49b33bf2a3eea1ef9f1f6
Colin Page <cwpage@umich.edu>
2020-04-22 16:11:15 -0400

    Record start and end of maze from user

--- MazeSolver/Maze.py
+++ MazeSolver/Maze.py
@@ -1,6 +1,8 @@
 """Maze class that will hold the representation of the maze"""
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 # NOTE: need contingency for mazes that are unsolvable
 
@@ -9,8 +11,9 @@ class Maze:
         self.maze = self.load_photo(file_path)
 
         # FIXME: get user input to determine these values:
-        self.start_loc = (0, 0)
-        self.end_loc = (0, 0)
+        locs = self.get_start_end()
+        self.start_loc = locs[0]
+        self.end_loc = locs[1]
 
         # List of (x, y) coordinates that comprise the solution for the maze
         self.correct_path = []
@@ -22,6 +25,17 @@ class Maze:
         maze[maze <= 155] = 0
 
         return maze
+
+    def get_start_end(self):
+        print("Please click the start position of the maze, then the end position")
+        fig = plt.figure(frameon=False)
+        ax = plt.Axes(fig, [0., 0., 1., 1.])
+        ax.set_axis_off()
+        fig.add_axes(ax)
+        ax.imshow(self.maze, aspect='auto')
+        locs = fig.ginput(n=2, timeout=0)
+        plt.close()
+        return locs
 
     def display_solved_maze(self):
         maze_color = self.maze
